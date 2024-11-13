c2387459e33e7199addf43b0b8eb69ae509af143
Kateryna <kateryna.babachanakh1@etu.univ-lorraine.fr>
2024-11-13 22:42:01 +0100

    Complét classe Avion

--- src/Avion.java
+++ src/Avion.java
@@ -1,27 +1,43 @@
+import java.util.Random;
+
 public class Avion extends Thread {
 
     private String nom;
     private Aeroport aeroport;
 
     public Avion(String s) {
+        super(s);
         this.nom = s;
     }
 
     public void run() {
-        this.aeroport = Aeroport.getInstance();
-        System.out.println("Je suis avion" + this.nom + " sur aeroport " + this.aeroport);
-        // demande à l'aéroport de lancer le décollage immédiatement
-        this.aeroport.faireDecoller(this);
+        try {
+            this.aeroport = Aeroport.getInstance();
+            System.out.println("Je suis avion " + this.nom + " et je suis prêt à décoller.");
+
+            // Bloc synchronisé pour attendre que la piste soit libre et décoller
+            synchronized (this) {
+                while (!this.aeroport.autoriserAdecoller()) {
+                    System.out.println("Avion " + this.nom + " en attente de la piste libre.");
+                    wait();
+                }
+                // La piste est libre, on peut décoller
+                decoller();
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Avion " + this.nom + " interrompu.");
+        }
     }
 
-    public void decoller() {
-        System.out.println("Décollage avion" + this.nom + " sur aéroport " + this.aeroport);
-        // le décollage prend un certain temps : 1 seconde
+    public void decoller() throws InterruptedException {
         try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Décollage de l'avion " + this.nom);
+            int tempsDecollage = new Random().nextInt(1000) + 500;
+            Thread.sleep(tempsDecollage);
+            System.out.println("Avion " + this.nom + " a décollé et a utilisé la piste pendant " + tempsDecollage + "ms.");
+        } finally {
+            this.aeroport.libererPiste();
+            System.out.println("Avion " + this.nom + " a libéré la piste.");
         }
-        System.out.println("Fin décollage avion" + this.nom + " sur aéroport " + this.aeroport);
     }
 }
